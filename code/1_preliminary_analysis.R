6b4c5179ee4b9b9f2340485a8d1de76bb70aae39
joejonzz <j.atkinson@unsw.edu.au>
2022-08-04 09:11:16 +0200

    additional plots
    made additional plots for the presentation based on meeting. all traits site by treatment.

--- code/1_preliminary_analysis.R
+++ code/1_preliminary_analysis.R
@@ -1,6 +1,10 @@
 library(dplyr)
 library(readr)
 library(ggplot2)
+library(lme4)
+library(sjPlot)
+library(lmerTest)
+
 
 # read clean data
 
@@ -39,19 +43,36 @@ data <- left_join(data, select(sp_list, c(Species, `G/F`)), by = c("taxon" = "Sp
 data <- data %>% mutate(mean_thickness = rowMeans(select(., c(leaf_thickness_1_mm,leaf_thickness_2_mm,leaf_thickness_3_mm)), na.rm=T))
 
 
-xx<-data %>% group_by(siteID, taxon, experiment) %>% summarise(average_height = mean(plant_height), average_thickness = mean(mean_thickness))
+xx<-data %>% group_by(siteID, taxon, experiment) %>% summarise(average_height = mean(plant_height, na.rm = T), average_thickness = mean(mean_thickness, na.rm = T),
+                                                               average_leafarea = mean(leaf_area_cm2, na.rm = T), average_sla = mean(sla_cm2_g, na.rm = T))
 
 xx<-left_join(xx, select(sp_list, c(Species, `G/F`)), by = c("taxon" = "Species")) %>% rename(lifeform = `G/F`)
 
-# species average height by site
-ggplot(xx, aes(experiment, average_height, group = taxon, colour = taxon)) +geom_point() +geom_line(col = "grey") +facet_wrap(~factor(siteID, levels = c("Ulv", "Gud", "Skj")))
 
-# species average height by site and lifeform
-ggplot(xx, aes(experiment, average_height, group = taxon, colour = taxon)) +geom_point() +geom_line(col = "grey") +facet_wrap(~factor(siteID, levels = c("Ulv", "Gud", "Skj"))~lifeform)
+# ----------------------------------------- #
+# trait differences by experiment and sites #
+# ----------------------------------------- #
 
-# individual plant heights (not averaged) from raw data
+# plant height
+ggplot(data, aes(experiment, plant_height, fill = siteID)) +geom_boxplot() +facet_wrap(~siteID, scales = "free") + theme_classic() +
+  scale_fill_manual(values = c("#FF6F59","#AB92BF","#C2E9AF"))+labs(y = "Plant height (mm)", x = "Treatment")
+#ggsave("outputs/height_experiment_site.png", height = 4, width = 5)
 
-ggplot(yy, aes(experiment, plant_height, fill = lifeform)) +geom_boxplot() +facet_wrap(~siteID)
+# SLA
+ggplot(data, aes(experiment, mean_thickness, fill = siteID)) +geom_boxplot() +facet_wrap(~siteID, scales = "free") + theme_classic() +
+  scale_fill_manual(values = c("#FF6F59","#AB92BF","#C2E9AF"))+labs(y = "Mean leaf thickness (mm)", x = "Treatment")
+#ggsave("outputs/thickness_experiment_site.png", height = 4, width = 5)
+
+# SLA
+ggplot(data, aes(experiment, leaf_area_cm2, fill = siteID)) +geom_boxplot() +facet_wrap(~siteID, scales = "free") + theme_classic() +
+  scale_fill_manual(values = c("#FF6F59","#AB92BF","#C2E9AF"))+labs(y = "Leaf area (cm2)", x = "Treatment")
+#ggsave("outputs/leafarea_experiment_site.png", height = 4, width = 5)
+
+# SLA
+ggplot(data, aes(experiment, sla_cm2_g, fill = siteID)) +geom_boxplot() +facet_wrap(~siteID, scales = "free") + theme_classic() +
+  scale_fill_manual(values = c("#FF6F59","#AB92BF","#C2E9AF"))+labs(y = "Specific leaf area (cm2/g)", x = "Treatment")
+#ggsave("outputs/sla_experiment_site.png", height = 5, width = 5)
+
 
 # individual plant heights by species and site
 
@@ -61,7 +82,16 @@ ggplot(data, aes(experiment, plant_height, fill = siteID)) +geom_boxplot() +face
 
 ggplot(data, aes(experiment, mean_thickness, fill = siteID)) +geom_boxplot() +facet_wrap(~taxon, scales = "free") + theme_classic()
 
-# global mean difference between OTC and C by site
+# same but leaf area
+
+ggplot(data, aes(experiment, log(leaf_area_cm2), fill = siteID)) +geom_boxplot() +facet_wrap(~taxon, scales = "free") + theme_classic()
+
+# same but SLA
+
+ggplot(data, aes(experiment, sla_cm2_g, fill = siteID)) +geom_boxplot() +facet_wrap(~taxon, scales = "free") + theme_classic()
+
+
+# global mean difference between OTC and C by site - plant height
 
 ggplot(data, aes(siteID, plant_height, fill = experiment)) + geom_boxplot()+
   theme_classic()+
@@ -69,7 +99,23 @@ ggplot(data, aes(siteID, plant_height, fill = experiment)) + geom_boxplot()+
   labs(y = "Plant height (mm)", x = "Site", fill = "Treatment")+
   scale_fill_manual(values = c("#FF6F59","#AB92BF"))
 
-ggsave("outputs/global_mean.png", height = 5, width  = 5)
+# same but leaf area
+
+ggplot(data, aes(siteID, log(leaf_area_cm2), fill = experiment)) + geom_boxplot()+
+  theme_classic()+
+  #ggtitle("Bistorta vivipara") +
+  labs(y = "Leaf area (cm2)", x = "Site", fill = "Treatment")+
+  scale_fill_manual(values = c("#FF6F59","#AB92BF"))
+
+
+
+#ggsave("outputs/global_mean_leaf_area.png", height = 5, width  = 5)
+
+
+# --------------------------------------------------- #
+# create plots of individual species for presentation #
+# --------------------------------------------------- #
+# height
 
 data %>% filter(taxon %in% c("Bistorta vivipara", "Carex bigelowii", "Poa alpina", "Veronica alpina")) %>% ggplot(., aes(experiment, plant_height, fill = siteID)) +
   geom_boxplot() +
@@ -78,6 +124,9 @@ data %>% filter(taxon %in% c("Bistorta vivipara", "Carex bigelowii", "Poa alpina
   labs(y = "Plant height (mm)", x = "Treatment", fill = "Site")+
   scale_fill_manual(values = c("#FF6F59","#AB92BF","#C2E9AF"))
 
+#ggsave("outputs/cherry_picked_height.png", height = 6, width = 6)
+
+# thickness
 
 data %>% filter(taxon %in% c("Bistorta vivipara", "Carex bigelowii", "Poa alpina", "Veronica alpina")) %>% ggplot(., aes(experiment, mean_thickness, fill = siteID)) +
   geom_boxplot() +
@@ -86,7 +135,32 @@ data %>% filter(taxon %in% c("Bistorta vivipara", "Carex bigelowii", "Poa alpina
   labs(y = "Mean leaf thickness (mm)", x = "Treatment", fill = "Site")+ggtitle("THIQUE CLIQUE!!!!!")+
   scale_fill_manual(values = c("#FF6F59","#AB92BF","#C2E9AF"))
 
-ggsave("outputs/cherry_picked_thickness.png", height = 6, width = 6)
+#ggsave("outputs/cherry_picked_thickness.png", height = 6, width = 6)
+
+# leaf area
+
+data %>% filter(taxon %in% c("Bistorta vivipara", "Carex bigelowii", "Poa alpina", "Veronica alpina")) %>% ggplot(., aes(experiment, leaf_area_cm2, fill = siteID)) +
+  geom_boxplot() +# coord_trans(y = "log") +
+  theme_classic()+ facet_wrap(~taxon, scales = "free")+
+  #ggtitle("Bistorta vivipara") +
+  labs(y = "Leaf area (cm2)", x = "Treatment", fill = "Site")+
+  scale_fill_manual(values = c("#FF6F59","#AB92BF","#C2E9AF"))
+
+#ggsave("outputs/cherry_picked_leafarea.png", height = 6, width = 6)
+
+# (wet) SLA
+
+data %>% filter(taxon %in% c("Bistorta vivipara", "Carex bigelowii", "Poa alpina", "Veronica alpina")) %>% ggplot(., aes(experiment, sla_cm2_g, fill = siteID)) +
+  geom_boxplot() +# coord_trans(y = "log") +
+  theme_classic()+ facet_wrap(~taxon, scales = "free")+
+  #ggtitle("Bistorta vivipara") +
+  labs(y = "(Wet) Specific leaf area (cm2/g)", x = "Treatment", fill = "Site")+
+  scale_fill_manual(values = c("#FF6F59","#AB92BF","#C2E9AF"))
+
+#ggsave("outputs/cherry_picked_sla.png", height = 6, width = 6)
+
+
+
 
 zz<-left_join(zz, select(sp_list, c(Species, `G/F`)), by = c("taxon" = "Species")) %>% rename(lifeform = `G/F`)
 
@@ -97,28 +171,11 @@ ggplot(zz, aes(experiment, mean_thickness #, fill = lifeform
 )) +geom_boxplot() +facet_wrap(~siteID)
 
 
-library(lme4)
-library(sjPlot)
-library(lmerTest)
-
-
-hist(sqrt(data$plant_height))
-hist(log(data$mean_thickness))
-
 ft<-lmer(plant_height ~ 1+ (1|lifeform/taxon/siteID/experiment), data = data)
 summary(ft)
 
-
-
-
 ft<-lmer(average_thickness ~ experiment*lifeform + siteID+(experiment|taxon), data = xx)
 summary(ft)
-
-
-
-# calculate the average thickness
-
-# some numbers for Joshua for the presentation
 
 
 
